fc7f816c53f1e3ba43969bb07562f0874a30564a
Michael <doctorbf3@gmail.com>
2021-05-15 10:54:21 +0500

    добавил посты и группы провел тесты

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Group
+
+
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("text", "pub_date", "author")
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",)
+    # добавляем возможность фильтрации по дате
+    list_filter = ("pub_date",)
+
+class GroupAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("title", "slug", "description")
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("title",)
+
+
+admin.site.register(Post, PostAdmin)
+
+admin.site.register(Group, GroupAdmin)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,12 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -51,10 +54,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'yatube.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# задаём адрес директории, куда командой *collectstatic* будет собрана вся статика
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
